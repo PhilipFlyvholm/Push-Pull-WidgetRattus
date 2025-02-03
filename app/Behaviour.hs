94e97d0d777bbbb99b30aba2078ffb367b754969
Faurby <lasse_fk@hotmail.com>
2025-02-03 12:13:46 +0100

    feat: add initial discretize and timer

--- app/Behaviour.hs
+++ app/Behaviour.hs
@@ -1,10 +1,13 @@
 {-# OPTIONS -fplugin=WidgetRattus.Plugin #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Avoid lambda using `infix`" #-}
+{-# HLINT ignore "Avoid lambda" #-}
 
 module Behaviour where
 
 import Primitives
 import WidgetRattus
-import WidgetRattus.Signal hiding (map)
+import WidgetRattus.Signal
 import Prelude hiding (map)
 
 data Beh a = Beh !(Sig (Fun Time a))
@@ -13,4 +16,15 @@ timeBehaviour :: Beh Time
 timeBehaviour = Beh (Fun (box id) ::: never)
 
 map :: Box (a -> b) -> Beh a -> Beh b
-map f (Beh (x ::: xs)) = Beh (mapF f x ::: delay (let Beh (s) = map f (Beh (adv xs)) in s))
+map f (Beh (x ::: xs)) = Beh (mapF f x ::: delay (let Beh s = Behaviour.map f (Beh (adv xs)) in s))
+
+-- Not 100% correct since it only uses the inital time and not future times
+discretize :: Beh a -> C (Sig a)
+discretize (Beh sig) = do
+    t <- time
+    return $ WidgetRattus.Signal.map (box (\ ft -> apply ft t)) sig
+
+timer :: C (Beh NominalDiffTime)
+timer = do
+    startTime <- time
+    return $ Beh (Fun (box (\currentTime -> diffTime startTime currentTime)) ::: never)
