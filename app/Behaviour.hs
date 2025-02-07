c7d39d1f7087efc027344373eab3d0e5962242e9
Philip Flyvholm <philipflyvholm@gmail.com>
2025-02-07 14:08:43 +0100

    Feat: Added a set of combinators

--- app/Behaviour.hs
+++ app/Behaviour.hs
@@ -8,8 +8,8 @@ module Behaviour where
 
 import Primitives
 import WidgetRattus
-import WidgetRattus.Signal
-import Prelude hiding (map)
+import WidgetRattus.Signal hiding (switch, zipWith)
+import Prelude hiding (map, zipWith)
 
 newtype Beh a = Beh (Sig (Fun Time a))
 
@@ -47,3 +47,34 @@ elapsedTime = do
 withTime :: O (Time -> a) -> O a
 withTime delayed =
   delayC $ delay (let f = adv delayed in do f <$> time)
+
+switch :: Beh a -> O (Beh a) -> Beh a
+switch (Beh (x ::: xs)) d =
+  Beh $
+    x
+      ::: delay
+        ( case select xs d of
+            Fst xs' d' -> let (Beh b') = Behaviour.switch (Beh xs') d' in b'
+            Snd _ (Beh d') -> d'
+            Both _ (Beh d') -> d'
+        )
+
+zipWith :: (Stable a, Stable b) => Box (a -> b -> c) -> Beh a -> Beh b -> Beh c
+zipWith f (Beh (x ::: xs)) (Beh (y ::: ys)) =
+  Beh
+    ( app x y
+        ::: delay
+          ( let (Beh rest) =
+                  ( case select xs ys of
+                      Fst xs' lys -> zipWith f (Beh xs') (Beh (y ::: lys))
+                      Snd lxs ys' -> zipWith f (Beh (x ::: lxs)) (Beh ys')
+                      Both xs' ys' -> zipWith f (Beh xs') (Beh ys')
+                  )
+             in rest
+          )
+    )
+  where
+    app (K x') (K y') = K (unbox f x' y')
+    app (Fun x') (Fun y') = Fun (box (\t -> unbox f (unbox x' t) (unbox y' t)))
+    app (Fun x') (K y') = Fun (box (\t -> unbox f (unbox x' t) y'))
+    app (K x') (Fun y') = Fun (box (unbox f x' . unbox y'))
--- app/Event.hs
+++ app/Event.hs
@@ -2,12 +2,32 @@
 module Event where
 
 import Behaviour
-import Primitives (apply)
+import Primitives (Fun (K))
 import WidgetRattus
 import WidgetRattus.Signal
 
 newtype Ev a = Ev (O (Sig a))
 
+map :: Box (a -> b) -> Ev a -> Ev b
+map f (Ev sig) =
+  Ev
+    ( delay
+        ( let (x ::: xs) = adv sig
+           in unbox f x
+                ::: let (Ev rest) = Event.map f (Ev xs)
+                     in rest
+        )
+    )
+
+stepper :: Ev a -> a -> Beh a
+stepper event initial =
+  Beh (K initial ::: delay (let (Beh sig) = adv (stepperAwait event) in sig))
+
+stepperAwait :: Ev a -> O (Beh a)
+stepperAwait (Ev sig) =
+  delay (let (x ::: xs) = adv sig in Beh (K x ::: delay (let (Beh sig') = adv (stepperAwait (Ev xs)) in sig')))
+
+{-
 triggerAwait :: (Stable b) => Box (a -> b -> c) -> Ev a -> Beh b -> Ev (Maybe' c)
 triggerAwait f event behaviour = Ev (trig f event behaviour)
   where
@@ -26,3 +46,4 @@ triggerAwait f event behaviour = Ev (trig f event behaviour)
                       )
                 )
           )
+-}
